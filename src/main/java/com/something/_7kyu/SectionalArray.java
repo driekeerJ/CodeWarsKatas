5c9aafadc4375d825a2c665e51eadbfa68c7e1d3
Wal van der <jeroen.van.der.wal@rabobank.nl>
2019-03-20 09:32:04 +0100

    SectionalArray finished

--- src/main/java/com/something/_7kyu/SectionalArray.java
+++ src/main/java/com/something/_7kyu/SectionalArray.java
@@ -2,25 +2,61 @@ package com.something._7kyu;
 
 // https://www.codewars.com/kata/58ef87dc4db9b24c6c000092/train/java
 
-class SectionalArray {
-    static int[] sort(final int[] array, final int start, final int... length) {
-        int[] result = getFirstUnsorted(array, start);
-        int end = length.length != 0 ? length[0] : array.length;
+import java.util.Arrays;
 
-        for (int i = start; i < end; i++) {
-            // Hier moet de vergelijking gaan komen.
-            result[i] = array[i];
+import static java.lang.System.arraycopy;
+
+class BestPracticeSectionalArray {
+    static int[] sort(final int[] array, final int start, final int... length) {
+        if (length.length == 0){
+            Arrays.sort(array, start, array.length);
+        } else {
+            Arrays.sort(array, start, array.length - 1);
         }
+        return array;
+    }
+}
+
+class SectionalArray {
+    static int[] sort(final int[] array, final int startSortingPosition, final int... length) {
+        final int startArray = 0;
+        final int arrayLength = array.length;
+        final int lengthSorting = getLengthSorting(startSortingPosition, arrayLength, length);
+        final int pointAfterSorting = startSortingPosition + lengthSorting;
+        final int slotsAfterSorting = arrayLength - startSortingPosition - lengthSorting;
+        final int[] toBeSorted = new int[lengthSorting];
+        final int[] result = new int[array.length];
+
+        arraycopy(array, startArray, result, startArray, startSortingPosition);
+        arraycopy(array, startSortingPosition, toBeSorted, startArray, lengthSorting);
+        int[] sorted = sortInts(toBeSorted);
+        arraycopy(sorted, startArray, result, startSortingPosition, lengthSorting);
+
+        if (slotsAfterSorting > 0)
+            arraycopy(array, pointAfterSorting, result, pointAfterSorting, slotsAfterSorting);
 
         return result;
     }
 
-    private static int[] getFirstUnsorted(final int[] array, final int start) {
-        int[] result = new int[array.length];
-        for (int i = 0; i < start; i++) {
-            result[i] = array[i];
-        }
-        return result;
+    private static int getLengthSorting(final int startSortingPosition, final int arrayLength, final int[] length) {
+        if (lengthIsGiven(length))
+            if(lengthGivenNotOutsideArrayRange(startSortingPosition, arrayLength, length[0]))
+                return length[0];
+        return arrayLength - startSortingPosition;
+    }
+
+    private static boolean lengthGivenNotOutsideArrayRange(final int startSortingPosition, final int arrayLength, final int sortingLength) {
+        return sortingLength + startSortingPosition <= arrayLength;
+    }
+
+    private static boolean lengthIsGiven(final int[] length) {
+        return length.length > 0;
+    }
+
+    private static int[] sortInts(final int[] array) {
+        return Arrays.stream(array)
+                    .sorted()
+                    .toArray();
     }
 }
 
--- src/test/java/com/something/_7kyu/SectionalArrayTest.java
+++ src/test/java/com/something/_7kyu/SectionalArrayTest.java
@@ -15,8 +15,17 @@ public final class SectionalArrayTest {
     @Test public void basicTests() {
         test(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, new int[]{1, 2, 5, 7, 4, 6, 3, 9, 8}, 2);
         test(new int[]{1, 2, 5, 7, 4, 6, 3, 9, 8}, new int[]{1, 2, 5, 7, 4, 6, 3, 9, 8}, 8);
+
+    }
+
+    @Test public void testWithLength() {
         test(new int[]{9, 7, 1, 2, 3, 4, 5, 8, 6}, new int[]{9, 7, 4, 2, 5, 3, 1, 8, 6}, 2, 5);
         test(new int[]{1, 2, 5, 7, 4, 6, 3, 9, 8}, new int[]{1, 2, 5, 7, 4, 6, 3, 9, 8}, 8, 3);
+        test(new int[]{813484, 394252, 573412, 937826, 860715, 977893, 886265, 855090, 878475, 725565},
+                new int[]{813484, 394252, 573412, 937826, 860715, 977893, 886265, 855090, 878475, 725565},7 ,2);
+    }
+
+    @Test public void emptyLists() {
         test(new int[]{}, new int[]{}, 0);
     }
 }
